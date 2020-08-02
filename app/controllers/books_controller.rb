a0e296d50cc7c22a9277c6037541686b9fa80a15
vagrant <vagrant@localhost.localdomain>
2020-08-02 08:33:12 +0000

    [finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,7 +14,7 @@ before_action :authenticate_user!
 
   def create
     @book = Book.new(book_params)
-    @books = Book.all
+    @book.user_id = current_user.id
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
